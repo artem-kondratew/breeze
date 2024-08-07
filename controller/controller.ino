15b4ffa7c5967830321a6ac7b2a122213ab53f97
artem-kondratew <artemkondratev5@gmail.com>
2024-08-07 16:54:16 +0300

    add ppm for blue motors

--- controller/controller.ino
+++ controller/controller.ino
@@ -10,9 +10,6 @@
 #define MOTOR_0_DATA_PIN               10
 #define MOTOR_1_DATA_PIN               11
 
-#define ESC_0_POWER_PIN                 5
-#define ESC_1_POWER_PIN                 6
-
 #define MOTOR_0_MIN_PPM              1000
 #define MOTOR_0_REVERSE_PPM          1500
 #define MOTOR_0_MAX_PPM              1956
@@ -21,11 +18,17 @@
 #define MOTOR_1_REVERSE_PPM          1500
 #define MOTOR_1_MAX_PPM              1956
 
-#define MOTOR_0_REAL_REVERSE_PPM_NEG 1457
-#define MOTOR_0_REAL_REVERSE_PPM_POS 1517
+//#define MOTOR_0_REAL_REVERSE_PPM_NEG 1457
+//#define MOTOR_0_REAL_REVERSE_PPM_POS 1517
+//
+//#define MOTOR_1_REAL_REVERSE_PPM_NEG 1457
+//#define MOTOR_1_REAL_REVERSE_PPM_POS 1518
 
-#define MOTOR_1_REAL_REVERSE_PPM_NEG 1457
-#define MOTOR_1_REAL_REVERSE_PPM_POS 1518
+#define MOTOR_0_REAL_REVERSE_PPM_NEG 1460
+#define MOTOR_0_REAL_REVERSE_PPM_POS 1512
+
+#define MOTOR_1_REAL_REVERSE_PPM_NEG 1460
+#define MOTOR_1_REAL_REVERSE_PPM_POS 1511
 
 #define MOTOR_0_MIN_VEL MOTOR_0_REAL_REVERSE_PPM_POS - MOTOR_0_REVERSE_PPM
 #define MOTOR_0_MIN_VEL_REV MOTOR_0_REAL_REVERSE_PPM_NEG - MOTOR_0_REVERSE_PPM
@@ -41,16 +44,14 @@ void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(MOTOR_0_DATA_PIN, OUTPUT);
     pinMode(MOTOR_1_DATA_PIN, OUTPUT);
-    pinMode(ESC_0_POWER_PIN, INPUT);
-    pinMode(ESC_1_POWER_PIN, INPUT);
     
     digitalWrite(LED_BUILTIN, LOW);
     
     serial::init(CMD_SIZE, MSG_SIZE, Parser::callback);
     serial::connect();
     
-    motor0 = new Motor(0, MOTOR_0_MIN_PPM, MOTOR_0_MAX_PPM, MOTOR_0_REVERSE_PPM, MOTOR_0_MIN_VEL, MOTOR_0_MIN_VEL_REV, MOTOR_0_DATA_PIN, ESC_0_POWER_PIN);
-    motor1 = new Motor(1, MOTOR_1_MIN_PPM, MOTOR_1_MAX_PPM, MOTOR_1_REVERSE_PPM, MOTOR_1_MIN_VEL, MOTOR_1_MIN_VEL_REV, MOTOR_1_DATA_PIN, ESC_1_POWER_PIN);
+    motor0 = new Motor(0, MOTOR_0_MIN_PPM, MOTOR_0_MAX_PPM, MOTOR_0_REVERSE_PPM, MOTOR_0_MIN_VEL, MOTOR_0_MIN_VEL_REV, MOTOR_0_DATA_PIN);
+    motor1 = new Motor(1, MOTOR_1_MIN_PPM, MOTOR_1_MAX_PPM, MOTOR_1_REVERSE_PPM, MOTOR_1_MIN_VEL, MOTOR_1_MIN_VEL_REV, MOTOR_1_DATA_PIN);
     
     start_time = millis();
 }
@@ -60,8 +61,8 @@ void loop() {
     Motor::spin();
     serial::receive();
 
-    if (millis() - start_time >= 100) {
-        serial::ok();
-        start_time = millis();
-    }
+   if (millis() - start_time >= 100) {
+       serial::ok();
+       start_time = millis();
+   }
 }
