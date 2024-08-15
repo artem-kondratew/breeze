6c1f4783273b5058ebd5737faed8a5813a163dc8
artem-kondratew <artemkondratev5@gmail.com>
2024-08-16 02:32:28 +0300

    impl init once only

--- pc/python/joystick.py
+++ pc/python/joystick.py
@@ -55,6 +55,8 @@ class Joystick:
         
         self.verbose = True
 
+        self.is_initialized = False
+
     def __delete__(self):
         pygame.quit()
 
@@ -76,7 +78,10 @@ class Joystick:
                     self.allow_move = True
                 state[self.led_on_button_idx] = True if self.joystick.get_button(self.led_on_button) else False
                 state[self.led_off_button_idx] = True if self.joystick.get_button(self.led_off_button) else False
-                state[self.init_button_idx] = True if self.joystick.get_button(self.init_button) else False
+
+                if self.joystick.get_button(self.init_button) and self.is_initialized:
+                    state[self.init_button_idx] = True
+                    self.is_initialized = False
 
             if event.type == pygame.JOYBUTTONUP:
                 if not self.joystick.get_button(self.allow_button):
